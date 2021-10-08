aa06e169f8415163af2fc839d95491c4f608eed6
Woze Parrot <wozeparrot@gmail.com>
2021-10-08 10:43:10 -0400

    use overlay in flake

--- flake.nix
+++ flake.nix
@@ -4,31 +4,24 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          config.allowUnfree = true;
-        };
-      in
-      rec {
-        packages.luvi =
-          pkgs.stdenv.mkDerivation rec {
+    let
+      overlay = final: prev: {
+        wozey = rec {
+          luvi = prev.stdenv.mkDerivation rec {
             pname = "luvi";
             version = "v2.12.0";
-            src = pkgs.fetchgit {
+            src = prev.fetchgit {
               url = "https://github.com/luvit/luvi.git";
               rev = version;
               leaveDotGit = true;
               sha256 = "sha256-vBARlOyW+leh5d9gh/5LzqicKLpMNldgod4zobQ2Xac=";
             };
 
-            nativeBuildInputs = with pkgs; [
+            nativeBuildInputs = with prev; [
               git
               cmake
             ];
-            buildInputs = with pkgs; [
+            buildInputs = with prev; [
               openssl
             ];
 
@@ -42,26 +35,45 @@
               install -p build/luvi $out/bin/
             '';
           };
-        packages.luvit =
-          let
-            luvitSrc = pkgs.fetchgit {
-              url = "https://github.com/luvit/luvit.git";
-              rev = "2.17.0";
-              leaveDotGit = true;
-              sha256 = "sha256-1p67H/Na8G1OYVjRHokl/VeWJeyn5ssfoZZFo5kQFZQ=";
-            };
-          in
-          pkgs.writeShellScriptBin "luvit" ''
-            ${packages.luvi}/bin/luvi ${luvitSrc} -- "$@"
-          '';
+          luvit =
+            let
+              luvitSrc = prev.fetchgit {
+                url = "https://github.com/luvit/luvit.git";
+                rev = "2.17.0";
+                leaveDotGit = true;
+                sha256 = "sha256-1p67H/Na8G1OYVjRHokl/VeWJeyn5ssfoZZFo5kQFZQ=";
+              };
+            in
+            prev.writeShellScriptBin "luvit" ''
+              ${luvi}/bin/luvi ${luvitSrc} -- "$@"
+            '';
 
-        packages.wozey =
-          let
-            wozeySrc = builtins.toString ./.;
-          in
-          pkgs.writeShellScriptBin "wozey" ''
-            ${packages.luvit}/bin/luvit ${wozeySrc}/main.lua
-          '';
-      }
-    );
+          wozey =
+            let
+              wozeySrc = builtins.toString ./.;
+            in
+            prev.writeShellScriptBin "wozey" ''
+              ${luvit}/bin/luvit ${wozeySrc}/main.lua
+            '';
+        };
+      };
+    in
+    flake-utils.lib.eachDefaultSystem
+      (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            config.allowUnfree = true;
+            overlays = [ overlay ];
+          };
+        in
+        {
+          packages = with pkgs.wozey; {
+            inherit wozey;
+          };
+        }
+      ) // {
+      overlay = overlay;
+    };
 }
