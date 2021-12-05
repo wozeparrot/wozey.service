03b4750f9a2e2a8cf86e5ffd6e3f3eca6fa78255
Woze Parrot <wozeparrot@gmail.com>
2021-12-05 13:19:00 -0500

    cleanup flake

--- flake.nix
+++ flake.nix
@@ -1,66 +1,11 @@
 {
   description = "wozey.service flake";
 
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.naersk.url = "github:nix-community/naersk";
 
-  outputs = { self, nixpkgs, flake-utils }:
-    let
-      overlay = final: prev: {
-        wozey =
-          let
-            luvi = final.stdenv.mkDerivation rec {
-              pname = "luvi";
-              version = "v2.13.0";
-              src = prev.fetchgit {
-                url = "https://github.com/luvit/luvi.git";
-                rev = version;
-                leaveDotGit = false;
-                sha256 = "sha256-h9Xdm/+9X3AoqBj1LJftqn3+3PbdankfAJSBP3KnRgw=";
-              };
-
-              nativeBuildInputs = with prev; [
-                git
-                cmake
-              ];
-              buildInputs = with prev; [
-                openssl
-              ];
-
-              configurePhase = ''
-                echo ${version} > VERSION
-                make regular-shared
-              '';
-
-              installPhase = ''
-                mkdir -p $out/bin
-                install -p build/luvi $out/bin/
-              '';
-            };
-            luvit =
-              let
-                luvitSrc = prev.fetchgit {
-                  url = "https://github.com/luvit/luvit.git";
-                  rev = "2.18.1";
-                  leaveDotGit = false;
-                  sha256 = "sha256-nxvzfiHURbNMkEqjtpO5Ja+miwX/2JfUc7b29mIY1xs=";
-                };
-              in
-              final.writeShellScriptBin "luvit" ''
-                ${luvi}/bin/luvi ${luvitSrc} -- "$@"
-              '';
-          in
-          rec {
-            wozey =
-              let
-                wozeySrc = ./bot;
-              in
-              final.writeShellScriptBin "wozey" ''
-                export PATH="${prev.yt-dlp}/bin:${prev.ffmpeg}/bin:${prev.ffmpeg-normalize}/bin:${prev.busybox}/bin"
-                LD_LIBRARY_PATH="${prev.libopus}/lib:${prev.libsodium}/lib" WOZEY_ROOT=${wozeySrc} ${luvit}/bin/luvit ${wozeySrc}/main.lua "$@"
-              '';
-          };
-      };
-    in
+  outputs = { self, nixpkgs, flake-utils, naersk }:
     flake-utils.lib.eachDefaultSystem
       (
         system:
@@ -68,17 +13,80 @@
           pkgs = import nixpkgs {
             inherit system;
             config.allowUnfree = true;
-            overlays = [ overlay ];
           };
+          naersk-lib = naersk.lib."${system}";
+          luvi = pkgs.stdenv.mkDerivation rec {
+            pname = "luvi";
+            version = "v2.13.0";
+            src = pkgs.fetchgit {
+              url = "https://github.com/luvit/luvi.git";
+              rev = version;
+              leaveDotGit = false;
+              sha256 = "sha256-h9Xdm/+9X3AoqBj1LJftqn3+3PbdankfAJSBP3KnRgw=";
+            };
+
+            nativeBuildInputs = with pkgs; [
+              git
+              cmake
+            ];
+            buildInputs = with pkgs; [
+              openssl
+            ];
+
+            configurePhase = ''
+              echo ${version} > VERSION
+              make regular-shared
+            '';
+
+            installPhase = ''
+              mkdir -p $out/bin
+              install -p build/luvi $out/bin/
+            '';
+          };
+          luvit =
+            let
+              luvitSrc = pkgs.fetchgit {
+                url = "https://github.com/luvit/luvit.git";
+                rev = "2.18.1";
+                leaveDotGit = false;
+                sha256 = "sha256-nxvzfiHURbNMkEqjtpO5Ja+miwX/2JfUc7b29mIY1xs=";
+              };
+            in
+            pkgs.writeShellScriptBin "luvit" ''
+              ${luvi}/bin/luvi ${luvitSrc} -- "$@"
+            '';
         in
         rec {
-          packages = with pkgs.wozey; {
-            inherit wozey;
+          packages.wozey =
+            let
+              wozeySrc = ./bot;
+            in
+            pkgs.writeShellScriptBin "wozey" ''
+              export PATH="${pkgs.yt-dlp}/bin:${pkgs.ffmpeg}/bin:${pkgs.ffmpeg-normalize}/bin:${pkgs.busybox}/bin"
+              LD_LIBRARY_PATH="${pkgs.libopus}/lib:${pkgs.libsodium}/lib" WOZEY_ROOT=${wozeySrc} ${luvit}/bin/luvit ${wozeySrc}/main.lua "$@"
+            '';
+          packages.wozey-compute = naersk-lib.buildPackage {
+            pname = "wozey-compute";
+            root = ./compute;
           };
-          defaultPackages = packages.wozey;
-          defaultApp = packages.wozey;
+          defaultPackage = packages.wozey;
+          
+          apps.wozey = flake-utils.lib.mkApp {
+            drv = packages.wozey;
+          };
+          apps.wozey-compute = flake-utils.lib.mkApp {
+            drv = packages.wozey-compute;
+          };
+          defaultApp = apps.wozey;
+
+          devShell = pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [
+              luvi luvit
+              
+              cargo rustc
+              libtorch-bin
+            ];
+          };
         }
-      ) // {
-      overlay = overlay;
-    };
+      );
 }
