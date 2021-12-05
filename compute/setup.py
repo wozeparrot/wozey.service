b58c8bcddad27480b36d2c81ad7ad221078a4a63
Woze Parrot <wozeparrot@gmail.com>
2021-12-05 15:25:33 -0500

    use python instead

---
+++ compute/setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='wozey-compute',
+      version='0.1.0',
+      packages=find_packages(),
+      scripts=["wozey-compute"],
+     )
--- flake.nix
+++ flake.nix
@@ -3,9 +3,8 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.naersk.url = "github:nix-community/naersk";
 
-  outputs = { self, nixpkgs, flake-utils, naersk }:
+  outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem
       (
         system:
@@ -14,7 +13,6 @@
             inherit system;
             config.allowUnfree = true;
           };
-          naersk-lib = naersk.lib."${system}";
           luvi = pkgs.stdenv.mkDerivation rec {
             pname = "luvi";
             version = "v2.13.0";
@@ -65,9 +63,13 @@
               export PATH="${pkgs.yt-dlp}/bin:${pkgs.ffmpeg}/bin:${pkgs.ffmpeg-normalize}/bin:${pkgs.busybox}/bin"
               LD_LIBRARY_PATH="${pkgs.libopus}/lib:${pkgs.libsodium}/lib" WOZEY_ROOT=${wozeySrc} ${luvit}/bin/luvit ${wozeySrc}/main.lua "$@"
             '';
-          packages.wozey-compute = naersk-lib.buildPackage {
+          packages.wozey-compute = with pkgs.python3Packages; buildPythonApplication {
             pname = "wozey-compute";
-            root = ./compute;
+            version = "0.1.0";
+
+            src = ./compute;
+
+            propagatedBuildInputs = [ transformers pytorch bottle ];
           };
           defaultPackage = packages.wozey;
           
