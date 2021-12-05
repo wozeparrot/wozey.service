242ff3c45ec8f44cf85f6c8648a35edef57aa818
Woze Parrot <wozeparrot@gmail.com>
2021-12-05 18:07:48 -0500

    more models + cleaned up discord interface

--- bot/libs/features/ai.lua
+++ bot/libs/features/ai.lua
@@ -5,23 +5,6 @@ local time = require("discordia").Time
 
 local log = require("discordia").Logger(3, "%F %T")
 
-local chat_history = {}
-local chatting = false
-
-local function char_to_hex(c)
-    return string.format("%%%02X", string.byte(c))
-end
-  
-local function urlencode(url)
-    if url == nil then
-        return
-    end
-    url = url:gsub("\n", "\r\n")
-    url = url:gsub("([^%w _%%%-%.~])", char_to_hex)
-    url = url:gsub(" ", "+")
-    return url
-end
-
 return function(client) return {
     name = "AI",
     description = "Attempts to be a chattable bot",
@@ -29,13 +12,6 @@ return function(client) return {
         ["ai"] = {
             description = "Talk with the bot",
             exec = function(message)
-                -- check if we are already chatting
-                if chatting then
-                    message:addReaction("⭕")
-                    return
-                end
-                chatting = true
-
                 -- don't do anything if its ourself
                 if message.author == client.user then return end
                 if message.member == nil then return end
@@ -53,24 +29,24 @@ return function(client) return {
                     return
                 end
 
-                -- sanity fixes
-                if chat_history[message.guild.id] == nil then
-                    chat_history[message.guild.id] = ""
-                end
-
-                -- generate context
-                local context = chat_history[message.guild.id].."\n\n"..message.member.name..": "..message.content:gsub(";ai ", "", 1).."\nwozey:"
+                -- generate post data
+                local post_data = json.stringify({
+                    ["text"] = message.content:gsub(";ai ", "", 1),
+                    ["id"] = message.member.id
+                })
                 log:log(3, "[ai] "..message.member.name.." said: "..message.content:gsub(";ai ", "", 1))
 
                 -- stored response string
                 local response = ""
                 -- generate request
                 local req = http.request({
-                    hostname = "api.vicgalle.net",
-                    port = 5000,
-                    path = "/generate?token_max_length=512&temperature=1.0&top_p=0.8&stop_sequence=%0A&context="..urlencode(context),
+                    hostname = "localhost",
+                    port = 6769,
+                    path = "/chat",
                     method = "POST",
                     headers = {
+                        ["Content-Type"] = "application/json",
+                        ["Content-Length"] = #post_data,
                         ["Accept"] = "application/json"
                     },
                 }, function(res)
@@ -82,26 +58,60 @@ return function(client) return {
                     res:on("end", coroutine.wrap(function()
                         -- parse returned json
                         local json_response = json.parse(response)
-
-                        if json_response.text == nil or json_response.text:match("^%s*(.-)%s*$") == "" then
+                        if json_response == nil then
                             message:addReaction("❌")
                             return
                         end
 
-                        log:log(3, "[ai] wozey responds: "..json_response.text:match("^%s*(.-)%s*$"))
-                        -- update chat history
-                        chat_history[message.guild.id] = chat_history[message.guild.id].."\n\n"..message.member.name..": "..message.content:gsub(";ai ", "", 1).."\nwozey: "..json_response.text:match("^%s*(.-)%s*$")
+                        if json_response.reply == nil or json_response.reply == "" then
+                            message:addReaction("❌")
+                            return
+                        end
+
+                        log:log(3, "[ai] wozey responds: "..json_response.reply)
 
                         message:reply({
-                            content = json_response.text,
+                            content = json_response.reply,
                             reference = { message = message, mention = false },
                         })
-
-                        chatting = false
                     end))
                 end)
+                -- write post data
+                req:write(post_data)
                 -- finish request
                 req:done()
+            end
+        },
+        ["air"] = {
+            description = "Reset chat history with the bot",
+            exec = function(message)
+                -- don't do anything if its ourself
+                if message.author == client.user then return end
+                if message.member == nil then return end
+
+                -- generate post data
+                local post_data = json.stringify({
+                    ["id"] = message.member.id
+                })
+
+                -- generate request
+                local req = http.request({
+                    hostname = "localhost",
+                    port = 6769,
+                    path = "/reset",
+                    method = "POST",
+                    headers = {
+                        ["Content-Type"] = "application/json",
+                        ["Content-Length"] = #post_data,
+                        ["Accept"] = "application/json"
+                    },
+                }, function(res) end)
+                -- write post data
+                req:write(post_data)
+                -- finish request
+                req:done()
+
+                message:addReaction("✅")
             end
         },
     },
