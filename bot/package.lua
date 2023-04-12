60f43aa5ca6c885c7f45e7e3fbb2f568827750c6
Woze Parrot <wozeparrot@gmail.com>
2023-04-11 23:56:32 -0400

    feat: replacing discordia dep

--- bot/package.lua
+++ bot/package.lua
@@ -1,17 +1,16 @@
 return {
-    name = "wozeparrot/wozey.service",
-    version = "0.1.0",
-    description = "A Opinionated Discord Bot",
-    tags = { "discord", "discord-bot" },
-    license = "Apache2",
-    author = { name = "Woze Parrot", email = "wozeparrot@gmail.com" },
-    homepage = "https://github.com/wozeparrot/wozey.service",
-    dependencies = {
-        "SinisterRectus/discordia@2.11.2",
-        "creationix/coro-spawn@3.0.2",
-    },
-    files = {
-        "**.lua",
-        "!test*",
-    },
+	name = "wozeparrot/wozey.service",
+	version = "0.1.0",
+	description = "An Opinionated Discord Bot",
+	tags = { "discord", "discord-bot" },
+	license = "Apache2",
+	author = { name = "Woze Parrot", email = "wozeparrot@gmail.com" },
+	homepage = "https://github.com/wozeparrot/wozey.service",
+	dependencies = {
+		"creationix/coro-spawn@3.0.2",
+	},
+	files = {
+		"**.lua",
+		"!test*",
+	},
 }
